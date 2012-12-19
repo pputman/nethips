d77d05a5b30d02bcfb4b8881e276b33d6eb95d1e
Patrick <logik@dataromance.net>
2012-12-18 18:34:45 -0600

    redid layout

--- app/models/child.rb
+++ app/models/child.rb
@@ -4,6 +4,7 @@ class Child < ActiveRecord::Base
 
   jtable :basic, :ovc_id, :name, :address, :contact_no, :age, :sex, :hiv_status, :weight, :treatment_code_of_parent, :treatment_type, :support_group, :location, :school, :class_form, :support_received, :caregiver_name, :plcode, :caregiver_relationship, :caregiver_contact_no, :caregiver_address, :caregiver_age, :caregiver_occupation
 
+
   def jtable_basic_attribute_picture
     if self.picture.blank?
 
